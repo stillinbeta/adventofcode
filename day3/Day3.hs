101aeb82c21f1ec32efe2640048b01ac410677c3
liz <liz@heroku.com>
2015-12-07 10:33:17 -0500

    Day 3 part b

--- day3/Day3.hs
+++ day3/Day3.hs
@@ -1,24 +1,29 @@
 module Day3 (santaRun) where
 
+import Text.Printf
+import qualified Data.Sequence as Seq
 import qualified Data.Set as Set
 
 data Point = Pt Int Int deriving (Eq, Ord)
 
-data SantaState = SantaState (Set.Set Point) Point
+data SantaState = SantaState (Set.Set Point) (Seq.Seq Point) Int
 
+baseSantaState :: Int -> SantaState
+baseSantaState ct = SantaState (Set.singleton pt) (Seq.replicate ct pt) 0 where
+                    pt = Pt 0 0
 
-baseSantaState = SantaState (Set.singleton pt) pt where
-                 pt = Pt 0 0
-
-santaRun :: String -> Int
-santaRun run = runLength $ foldl santaMove baseSantaState run
+santaRun :: Int -> String -> Int
+santaRun santas run  = runLength $ foldl santaMove (baseSantaState santas) run
 
 runLength :: SantaState -> Int
-runLength (SantaState set _) = Set.size set
+runLength (SantaState set _ _) = Set.size set
 
 santaMove :: SantaState -> Char -> SantaState
-santaMove (SantaState set pos) chr = let pos' = move pos chr in
-                                     SantaState (Set.insert pos' set) pos'
+santaMove (SantaState set seq ct) chr = let idx = ct `mod` (Seq.length seq)
+                                            pos = Seq.index seq idx
+                                            pos' = move pos chr
+                                            seq' = Seq.update idx pos' seq in
+                                     SantaState (Set.insert pos' set) seq' (idx + 1)
 
 move :: Point -> Char -> Point
 move (Pt x y) '>' = Pt (x+1) y
@@ -27,4 +32,6 @@ move (Pt x y) '^' = Pt x (y+1)
 move (Pt x y) 'v' = Pt x (y-1)
 move point _ = point
 
-main = interact $ show . santaRun
+main = interact $ \input ->
+    (printf "With 1 santa: %d\n"  $ santaRun 1 input) ++
+    (printf "With 2 santas: %d\n" $ santaRun 2 input)
--- day3/Day3Test.hs
+++ day3/Day3Test.hs
@@ -3,13 +3,21 @@ module Day3Test where
 import Test.HUnit
 import Day3
 
-test1 = TestCase (assertEqual path 2 $ santaRun(path)) where
+test1 = TestCase (assertEqual path 2 $ santaRun 1 path) where
             path = ">"
-test2 = TestCase (assertEqual path 4 $ santaRun(path)) where
+test2 = TestCase (assertEqual path 4 $ santaRun 1 path) where
             path = "^>v<"
-test3 = TestCase (assertEqual path 2 $ santaRun(path)) where
+test3 = TestCase (assertEqual path 2 $ santaRun 1 path) where
+            path = "^v^v^v^v^v"
+test4 = TestCase (assertEqual path 3 $ santaRun 2 path) where
+            path = "^v"
+test5 = TestCase (assertEqual path 3 $ santaRun 2 path) where
+            path = "^>v<"
+test6 = TestCase (assertEqual path 11 $ santaRun 2 path) where
             path = "^v^v^v^v^v"
 
-tests = TestList [ test1, test2, test3 ]
+
+
+tests = TestList [ test1, test2, test3, test4, test5, test6 ]
 
 main = runTestTT tests
