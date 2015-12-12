e8b8b026baff5715deca0def7c752f349c4b188c
liz <liz@heroku.com>
2015-12-11 22:34:25 -0500

    Day7 WIP

--- day7/Day7.hs
+++ day7/Day7.hs
@@ -1,25 +1,37 @@
-module Day7 (parse, Address(..), Operation(..), Instruction(..), Assignment(..)) where
+module Day7 (parse, Address(..), Operation(..), Instruction(..), Assignment(..), Wire(..)) where
 
-import Data.List.Split
-import Data.Word
+import Control.Monad.State.Lazy
+import Data.Bits
 import Data.Char
+import Data.Either
+import Data.List
+import Data.List.Split
 import qualified Data.Map.Strict as Map
+import Data.Word
 
 data WordState = WS { vowels           :: Int,
                       doubleLetter     :: Bool,
                       forbiddenStrings :: Bool }
 
 type Value = Word16
-data Address = Register String | Constant Value deriving (Eq, Show)
+newtype Wire = Wire String deriving (Eq, Show, Ord)
+data Address = Source Wire | Constant Value deriving (Eq, Show)
 data Operation = And | Or | LShift | RShift deriving (Eq, Show)
 data Instruction = Val Address | Not Address | Op Operation Address Address deriving (Eq, Show)
-data Assignment = Assign Instruction Address deriving (Eq, Show)
+data Assignment = Assign Instruction Wire deriving (Eq, Show)
+
+type Circuit = Map.Map Wire Instruction
+
+-- Begin parsing
 
 isConstant :: String -> Bool
 isConstant = foldr ((&&) . isNumber) True
 
-isRegister :: String -> Bool
-isRegister = foldr ((&&) . isLower) True
+isWire :: String -> Bool
+isWire = foldr ((&&) . isLower) True
+
+isAddress :: String -> Bool
+isAddress = foldr ((&&) . isAlphaNum) True
 
 toOp :: String -> Either String Operation
 toOp "AND" = Right And
@@ -29,30 +41,89 @@ toOp "RSHIFT" = Right RShift
 toOp op = Left $ "Can't parse op \"" ++ op ++ "\""
 
 -- not strictly correct (should be all alpha or all num), but eh.
-isAddress :: String -> Bool
-isAddress = foldr ((&&) . isAlphaNum) True
+toWire :: String -> Either String Wire
+toWire w | isWire w = Right $ Wire w
+toWire w = Left $ "can't parse \"" ++ w ++"\" to wire"
 
 toAddress :: String -> Either String Address
 toAddress a | isConstant a = Right $ Constant (read a)
-toAddress a | isRegister a = Right $ Register a
+toAddress a | isWire a = Right $ Source (Wire a)
 toAddress a = Left $ "can't parse \"" ++ a ++ "\"to address"
 
 parse :: String -> Either String Assignment
-parse = parse' . (splitOn " ")
+parse = parse' . words
 
 parse' :: [String] -> Either String Assignment
 parse' [a1, op, a2, "->", a3] = do
     a1' <- toAddress a1
     a2' <- toAddress a2
-    a3' <- toAddress a3
+    a3' <- toWire a3
     op' <- toOp op
     return $ Assign (Op op' a1' a2') a3'
 parse' ["NOT", a1, "->", a2] = do
     a1' <- toAddress a1
-    a2' <- toAddress a2
+    a2' <- toWire a2
     return $ Assign (Not a1') a2'
 parse' [a1, "->", a2] = do
     a1' <- toAddress a1
-    a2' <- toAddress a2
+    a2' <- toWire a2
     return $ Assign (Val a1') a2'
 parse' xs = Left $ "can't parse " ++ show xs
+
+-- End parsing
+-- Begin Solving
+
+--solve :: [Assignment] -> Circuit
+
+--solve' :: [
+
+--fillIn :: [Wire] -> State Circuit Boolean
+--fillIn (w:ws) = --do
+--    set <- getState
+
+getConstant :: Address -> Circuit -> Maybe Value
+getConstant (Source wire) circuit = do
+    instr <- Map.lookup wire circuit
+    (case instr of
+        (Val (Constant val)) -> Just val
+        otherwise -> Nothing)
+getConstant (Constant value) _ = Just value
+
+eval :: Instruction -> Circuit -> Maybe Value
+eval (Val a) circuit = do
+    val <- getConstant a circuit
+    return val
+eval (Not a) circuit = do
+    val <- getConstant a circuit
+    return $ complement val
+eval (Op op a1 a2) circuit = do
+    val1 <- getConstant a1 circuit
+    val2 <- getConstant a2 circuit
+    return $ case op of
+        And -> val1 .&. val2
+        Or ->  val1 .|. val2
+        RShift -> val1 `shiftR` (fromIntegral val2)
+        LShift -> val1 `shiftL` (fromIntegral val2)
+
+backfill :: Wire -> Circuit -> Circuit
+backfill wire circuit =
+    let newInstr = do
+        instr <- Map.lookup wire circuit
+        val <- eval instr circuit
+        return val in
+    case newInstr of
+        Just v -> Map.insert wire (Val (Constant v)) circuit
+        Nothing -> circuit
+
+constructCircuit :: [Assignment] -> Circuit
+constructCircuit ((Assign instr wire):as) = Map.insert wire instr $ constructCircuit as
+constructCircuit [] = Map.empty
+
+-- End Solving
+-- Begin execution
+main = do
+    file <- getContents
+    let results = map parse $ splitOn "\n" file
+    putStrLn $ intercalate "\n" $ lefts results
+
+-- End execution
--- day7/Day7Test.hs
+++ day7/Day7Test.hs
@@ -4,19 +4,19 @@ import Test.HUnit
 import Day7
 
 test1 = TestCase (assertEqual line (Right expected) $ parse line) where
-            expected = Assign (Val (Constant 123)) (Register "x")
+            expected = Assign (Val (Constant 123)) (Wire "x")
             line = "123 -> x"
 
 test2 = TestCase (assertEqual line (Right expected) $ parse line) where
-            expected = Assign (Op And (Register "x") (Register "y")) (Register "z")
+            expected = Assign (Op And (Source (Wire "x")) (Source (Wire "y"))) (Wire "z")
             line = "x AND y -> z"
 
 test3 = TestCase (assertEqual line (Right expected) $ parse line) where
-            expected = Assign (Op LShift (Register "p") (Constant 2)) (Register "q")
+            expected = Assign (Op LShift (Source (Wire "p")) (Constant 2)) (Wire "q")
             line = "p LSHIFT 2 -> q"
 
 test4 = TestCase (assertEqual line (Right expected) $ parse line) where
-            expected = Assign (Not (Register "e")) (Register "f")
+            expected = Assign (Not (Source (Wire "e"))) (Wire "f")
             line = "NOT e -> f"
 
 tests = TestList [ test1, test2, test3, test4 ]
