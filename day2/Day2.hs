94267f43ba1916bd8f2dec9d720fcb48bc54dc62
liz <liz@heroku.com>
2015-12-12 12:19:43 -0500

    Day 2 part b

--- day2/Day2.hs
+++ day2/Day2.hs
@@ -1,25 +1,30 @@
-module Day2 ( presentArea, allPresentArea ) where
+module Day2 (presentParser, presentArea, ribbonLength , Present(Present)) where
 
+import Data.List
 import Data.List.Split
+import Data.Maybe
 
-data Present = Present Int Int Int
+data Present = Present Int Int Int deriving Show
 
-presentArea :: String -> Int
-presentArea = presentArea' . presentParser
-
-presentParser :: String -> Present
+presentParser :: String -> Maybe Present
 presentParser present = case splitOn "x" present of
-                            [l, w, h] -> Present (read l) (read w) (read h)
-                            _ -> Present 0 0 0 -- trailing newline
-                            -- _ -> error $ "can't parse" ++ present
+                            [l, w, h] -> Just $ Present (read l) (read w) (read h)
+                            _ -> Nothing
 
-presentArea' :: Present -> Int
-presentArea' (Present l w h) = let prod1 = l * w
-                                   prod2 = w * h
-                                   prod3 = l * h
-                               in minimum [prod1, prod2, prod3] + 2 * (prod1 + prod2 + prod3)
+presentArea :: Present -> Int
+presentArea (Present l w h) = let prod1 = l * w
+                                  prod2 = w * h
+                                  prod3 = l * h in
+                               minimum [prod1, prod2, prod3] + 2 * (prod1 + prod2 + prod3)
 
-allPresentArea :: String -> Int
-allPresentArea presents = sum ( map presentArea $ splitOn "\n" presents )
+ribbonLength :: Present -> Int
+ribbonLength (Present l w h) = let (d1:d2:_) = sort [l, w, h] in
+                            2 * (d1 + d2) + l * w * h
 
-main = interact $ show . allPresentArea
+main = do
+    contents <- getContents
+    let presents = catMaybes $ map presentParser $ lines contents
+    let allPresentArea = foldr ((+) . presentArea) 0 presents
+    putStrLn $ "part a: " ++ show allPresentArea
+    let allRibbonLength = foldr ((+) . ribbonLength) 0 presents
+    putStrLn $ "part b: " ++ show allRibbonLength
--- day2/Day2Test.hs
+++ day2/Day2Test.hs
@@ -3,12 +3,16 @@ module Day2Test where
 import Test.HUnit
 import Day2
 
-test1 = TestCase (assertEqual area 58 $ presentArea(area))
+test1 = TestCase (assertEqual area 58 $ (presentArea . presentParser) area)
         where area = "2x3x4"
-test2 = TestCase (assertEqual area 43 $ presentArea(area))
-        where area = "1x1x10"
-test3 = TestCase (assertEqual "multiline" (58 + 43) $ allPresentArea("2x3x4\n1x1x10"))
+test2 = TestCase (assertEqual area 43 $ (presentArea . presentParser) area)
 
-tests = TestList [ test1, test2, test3 ]
+test4 = TestCase (assertEqual (show present) 34 $ ribbonLength present) where
+                present = Present 2 3 4
+
+test5 = TestCase (assertEqual (show present) 14 $ ribbonLength present) where
+                present = Present 1 1 10
+
+tests = TestList [ test1, test2, test4, test5 ]
 
 main = runTestTT tests
