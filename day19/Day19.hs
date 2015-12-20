37a103f7376ac69eba227408d81fde590f8bd634
liz <liz@heroku.com>
2015-12-19 22:11:49 -0500

    More day 19 WIP

--- day19/Day19.hs
+++ day19/Day19.hs
@@ -6,6 +6,8 @@ import Data.Maybe (maybeToList, listToMaybe, mapMaybe)
 import qualified Data.Map.Strict as Map
 import Text.ParserCombinators.Parsec hiding (State)
 
+import Debug.Trace
+
 import Data.Char (isLower)
 
 type Calibration = Map.Map String [String]
@@ -18,12 +20,12 @@ p_input = do
             Map.insertWith (++) k [v] m) Map.empty lines)
 
 p_calibration = do
-        alias <- try (p_chemical) <|> string "e"
+        alias <- try p_chemical
         string " => "
         value <- p_chemicals
         return (alias, value)
 
-p_chemical = do
+p_chemical = try (string "e") <|> do
         letter <- upper
         letter2 <- try (optionMaybe lower)
         return $ letter:(maybeToList letter2)
@@ -31,6 +33,11 @@ p_chemical = do
 p_chemicals = do
         chemicals <- many1 p_chemical
         return $ concat chemicals
+
+p_chemicalPlus = do
+        chemical <- p_chemical
+        rest <- many letter
+        return (chemical, rest)
 
 parseCalibration :: String -> Either ParseError (String, Calibration)
 parseCalibration = parse p_input "(unknown)"
@@ -44,7 +51,7 @@ substitutionCount' :: String -> String -> Calibration -> State (Set.Set String) 
 substitutionCount' _ [] _ = return ()
 
 substitutionCount' prev xs calibration = do
-        let (key, next) = case xs of
+        let (key, next) = case xs of 
                       (x1:x2:xs) -> if isLower x2
                                         then (x1:x2:[], xs)
                                         else (x1:[], x2:xs)
@@ -55,18 +62,38 @@ substitutionCount' prev xs calibration = do
             Nothing -> pure ()
         substitutionCount' (prev ++ key) next calibration
 
+commonPrefix :: (Eq a) => [a] -> [a] -> ([a], [a], [a])
+commonPrefix (x1:xs1) (x2:xs2) | x1 /= x2 = ([], x1:xs1, x2:xs2)
+                               | x1 == x2 =
+                       let (cs1, xs1', xs2') = commonPrefix xs1 xs2 in
+                           (x1:cs1, xs1', xs2')
+commonPrefix [] xs2 = ([], [], xs2)
+commonPrefix xs1 [] = ([], xs1, [])
+
+commonPrefix1 :: (Eq a) => [a] -> [a] -> ([a], [a], [a])
+commonPrefix1 (x1:xs1) (x2:xs2) | x1 /= x2 = ([], x1:xs1, x2:xs2)
+                                | x1 == x2 = ([x1], xs1, xs2)
+commonPrefix1 [] xs2 = ([], [], xs2)
+commonPrefix1 xs1 [] = ([], xs1, [])
+
+getChemical :: String -> Maybe (String, String)
+getChemical string = case parse p_chemicalPlus "" string of
+                         Left _ -> Nothing
+                         Right res -> Just res
+
 searchDepth :: String -> Calibration -> Maybe Int
 searchDepth query calibration = searchDepth' 0 query calibration "e"
 
 searchDepth' :: Int -> String -> Calibration -> String -> Maybe Int
-searchDepth' 30 _ _ _ = Nothing
-searchDepth' depth query calibration curr | query == curr = Just depth
+searchDepth' depth query calibration curr | length query < length curr = Nothing
                                           | otherwise =
-          let set = execState (substitutionCount' "" curr calibration) Set.empty
-              words = Set.toList set in
-                  listToMaybe $ mapMaybe (searchDepth' (depth + 1) query calibration) words
-
-
+          case traceShow (curr, query) $ traceShowId $ commonPrefix1 query curr of
+                  (_, [], []) -> Just depth
+                  (_, query', curr') -> do
+                      (chem, rest) <- getChemical curr'
+                      subts <- Map.lookup chem calibration
+                      let f = (searchDepth' (depth + 1) query' calibration) . (++rest)
+                      listToMaybe $ mapMaybe f subts
 main = do
     contents <- getContents
     case parseCalibration contents of
